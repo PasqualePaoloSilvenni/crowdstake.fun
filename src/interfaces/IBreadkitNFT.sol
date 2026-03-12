e36f09ded25f27acae550a9b97b26b2057812acd
PasqualePaoloSilvenni <p.silvenni@studenti.unina.it>
2026-03-12 16:57:31 +0100

    feat: add standalone DistributionNFTModule for Issue #24

---
+++ src/interfaces/IBreadkitNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+
+/// @title Interface for the Breadkit NFT contract
+/// @dev Extends IERC721 by adding the mint and burn functions required by the module.
+interface IBreadkitNFT is IERC721 {
+    /// @notice Mints a new NFT to the specified address.
+    /// @param to The address that will receive the NFT.
+    /// @return tokenId The unique ID of the newly minted NFT.
+    function mint(address to) external returns (uint256);
+
+    /// @notice Burns a specific NFT.
+    /// @param tokenId The ID of the NFT to burn.
+    function burn(uint256 tokenId) external;
+}
