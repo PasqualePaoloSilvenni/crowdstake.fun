7d8cfd1247fb41d1d42f70d6ee46b7d5d538b391
PasqualePaoloSilvenni <p.silvenni@studenti.unina.it>
2026-03-31 20:08:19 +0200

    chore: remove accidental junk files
    Deleted unintended files like 'block', 'nonce', 'total', and 'used' that were accidentally generated locally.

--- src/interfaces/ICrowdstakeNFT.sol
+++ src/interfaces/ICrowdstakeNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /// @title Interface for the Crowdstake NFT contract
-/// @dev Extends IERC721 by adding the mint and burn functions required by the module.
+/// @dev Extends IERC721 by adding mint and burn functions used by the Crowdstake distribution strategy and other external consumers.
 interface ICrowdstakeNFT is IERC721 {
     /// @notice Mints a new NFT to the specified address.
     /// @param to The address that will receive the NFT.
